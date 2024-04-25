teedine/telegram-channel

commit 8f194e284c281b584fb286fc57d433c98d06bd19

quality of life changes

main.go
@@ -70,7 +70,12 @@ func videoWatch(Bot *telegram.Bot, cs Config, filepath string) {
 	}
 
 	dir := path.Dir(filepath)
-	if dir == cs.UploadFromPath && file.GetSize(filepath) > 512 {
+	if dir == cs.UploadFromPath {
+		if file.GetSize(filepath) > 512 {
+			fmt.Println("ignoring video below 512 bytes")
+			return
+		}
+
 		channelID, _ := strconv.Atoi(cs.ChannelID)
 
 		rsp, err := Bot.SendVideo(filepath, int64(channelID))

file/file.go
@@ -129,6 +129,7 @@ func cleanPath(s string) string {
 	return c
 }
 
+// returns size in bytes
 func GetSize(s string) int {
 	f, _ := os.Stat(s)
 	return int(f.Size())

telegram/telegram.go
@@ -26,6 +26,7 @@ func (b *Bot) SendVideo(r string, channelID int64) (tg.Message, error) {
 	c := tg.NewVideoUpload(channelID, r)
 	c.BaseChat.DisableNotification = true
 
+	fmt.Println("uploading", r)
 	msg, err := b.T.Send(c)
 	if err != nil {
 		return tg.Message{}, err

commit df4fb1b0f8405e6f9c37fa35f50717d4d25ec74e

fixed checking file size incorrectly (introduced by 8f194e2 (quality of life changes, 2024-04-24))

main.go
@@ -79,7 +79,7 @@ func videoWatch(Bot *telegram.Bot, cs Config, filepath string) {
 
 	dir := path.Dir(filepath)
 	if dir == cs.UploadFromPath {
-		if file.GetSize(filepath) > 512 {
+		if file.GetSize(filepath) <= 512 {
 			fmt.Println("ignoring video below 512 bytes")
 			return
 		}